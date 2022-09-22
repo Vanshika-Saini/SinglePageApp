d7aef2d56d2598d651590ca1dcc033d0fa121661
Vanshika Saini <2002vanshika.saini@gmail.com>
2022-09-22 15:34:48 +0300

    commit

---
+++ src/Components/Home.js
@@ -0,0 +1,9 @@
+export default function home(){
+    return (
+        <div>
+            This is Home.
+        </div>
+
+    );
+}
+
---
+++ src/Components/NotFound.js
@@ -0,0 +1,10 @@
+function NotFound(){
+    return (
+        <div>
+            This page is Not Found.
+        </div>
+
+    );
+}
+
+export default Home;
